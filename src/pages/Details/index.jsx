220947a5d978e24326a9904a70c0a44799118d68
Eduardo Storari <71531371+EduardoStorari@users.noreply.github.com>
2023-02-22 22:18:12 -0300

    Second commit

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,4 +1,5 @@
 import {Container} from "./styles";
+import {Button } from "../../components/button";
 
 export function Details() {
 
@@ -6,6 +7,10 @@ export function Details() {
     <Container>
       <h1>Hello World</h1>
       <span>Eduardo Storari</span>
+
+      <Button title ="wow" loading/>
+      <Button title ="genius"/>
+      <Button title ="wow!!!"/>
     </Container>   
   )
 };
